81989bdea4f77a959ee6791d83a80e9cf62c7f90
Marek Marcola <Marek.Marcola@gmail.com>
2025-12-04 23:53:42 +0100

    vdbench: change test dir to current dir
    Change-Id: Ie5582c76017c8af2f239cafac41265caa81fe0b0

--- vdbench/scripts/vdb.sh
+++ vdbench/scripts/vdb.sh
@@ -2,18 +2,18 @@
 
 if [ "$1" = "-r" ]; then
   ID=vdb-r-10gb
-  echo -e "\n$(hostname -s)-$(date +%Y%m%d%H%M)" >> /vdbench/${ID}.txt
-  vdbench -f $ID.conf -o /vdbench/$ID | \
+  echo -e "\n$(hostname -s)-$(date +%Y%m%d%H%M)" >> $ID.txt
+  vdbench -f /usr/local/etc/vdbench/$ID.conf -o $ID | \
     grep --line-buffered -E 'resp|avg' | \
     sed --unbuffered -e '1p' -e '2p' -e '/avg/!d' | \
-    tee -a /vdbench/${ID}.txt
+    tee -a $ID.txt
 elif [ "$1" = "-w" ]; then
   ID=vdb-w-10gb
-  echo -e "\n$(hostname -s)-$(date +%Y%m%d%H%M)" >> /vdbench/${ID}.txt
-  vdbench -f $ID.conf -o /vdbench/$ID | \
+  echo -e "\n$(hostname -s)-$(date +%Y%m%d%H%M)" >> $ID.txt
+  vdbench -f /usr/local/etc/vdbench/$ID.conf -o $ID | \
     grep --line-buffered -E 'resp|avg' | \
     sed --unbuffered -e '1p' -e '2p' -e '/avg/!d' | \
-    tee -a /vdbench/${ID}.txt
+    tee -a $ID.txt
 else
   echo "$(basename $0) -r|-w # read,write"
 fi

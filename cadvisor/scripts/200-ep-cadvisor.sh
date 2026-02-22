6ad8e427161b43737963ceffe746ab7739c13492
Marek Marcola <Marek.Marcola@gmail.com>
2026-02-22 01:12:13 +0100

    cadvisor: replace __ to space in CADVISOR_OPTS env
    Change-Id: I102643018a3d4d4a84d6ee6b097c46430dee35b5

--- cadvisor/scripts/200-ep-cadvisor.sh
+++ cadvisor/scripts/200-ep-cadvisor.sh
@@ -2,7 +2,7 @@
 
 [[ -z $CADVISOR_OPTS ]] && CADVISOR_OPTS=""
 
-CADVISOR_OPTS=$(echo $CADVISOR_OPTS|sed 's/,/ /g')
+CADVISOR_OPTS=$(echo $CADVISOR_OPTS|sed 's/__/ /g')
 
 echo "env config:"
 echo "    CADVISOR_OPTS = $CADVISOR_OPTS"

263577535bf1d79ab7cfb6b921b1a7bb6860db8f
Marek Marcola <Marek.Marcola@gmail.com>
2026-03-21 23:23:47 +0100

    go2rtc: add local config file
    Change-Id: Icd800199627bcda85b7cf4e260e0510897e9dd50

--- go2rtc/scripts/200-ep-go2rtc.sh
+++ go2rtc/scripts/200-ep-go2rtc.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-[[ -z $GO2RTC_ID   ]] && GO2RTC_ID=""
-[[ -z $GO2RTC_CONF ]] && GO2RTC_CONF=/usr/local/etc/go2rtc/go2rtc${GO2RTC_ID=:+-}${GO2RTC_ID}.yaml
+[[ -z $GO2RTC_CONF ]] && GO2RTC_CONF=/usr/local/etc/go2rtc
 
 echo "env config:"
-echo "    GO2RTC_ID   = $GO2RTC_ID"
 echo "    GO2RTC_CONF = $GO2RTC_CONF"
 echo
+
+if [ ! -d $DNSMASQ_CONF ]; then
+  echo ${0##*/}: error: no config directory: $DNSMASQ_CONF
+  exit 1
+fi
 
 set -x
 go2rtc -v
 exec setpriv --reuid=666 --regid=666 --clear-groups --no-new-privs \
-  go2rtc -c $GO2RTC_CONF
+  go2rtc -c $GO2RTC_CONF/go2rtc-local.yaml -c $GO2RTC_CONF/go2rtc.yaml

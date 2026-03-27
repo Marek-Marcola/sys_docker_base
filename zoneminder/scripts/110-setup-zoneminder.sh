37c56d2e292ff6891745a5166349d8f6233fe28f
Marek Marcola <Marek.Marcola@gmail.com>
2026-03-27 19:11:40 +0100

    zoneminder: add software for ffmpeg vaapi hardware accelerator
    Change-Id: I16e0566fbc76e839b7e495fb98e6db30f224744a

--- zoneminder/scripts/110-setup-zoneminder.sh
+++ zoneminder/scripts/110-setup-zoneminder.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-set -x
-chmod -v a+rw /dev/video*
-chmod -v a+rw /dev/media*
+if [ -n "$(ls /dev/video* 2>/dev/null)" ]; then
+  set -x
+  chmod -v a+rw /dev/video*
+  { set +ex; } 2>/dev/null
+fi
+
+if [ -n "$(ls /dev/media* 2>/dev/null)" ]; then
+  set -x
+  chmod -v a+rw /dev/media*
+  { set +ex; } 2>/dev/null
+fi
+
+if [ -n "$(ls /dev/dri/* 2>/dev/null)" ]; then
+  set -x
+  chmod -v a+rw /dev/dri/*
+  { set +ex; } 2>/dev/null
+fi

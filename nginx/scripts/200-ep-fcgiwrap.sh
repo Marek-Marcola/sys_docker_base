9b5db4701300a313990f836b747ddc62063d02ba
Marek Marcola <Marek.Marcola@gmail.com>
2026-07-29 01:28:21 +0200

    nginx: add FCGIWRAP_SOCKET option to 200-ep-fcgiwrap.sh script
    Change-Id: I974c44f597e7dcb18d05da54577545e21236b836

--- nginx/scripts/200-ep-fcgiwrap.sh
+++ nginx/scripts/200-ep-fcgiwrap.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-[[ -z $FCGIWRAP_PREFORK   ]] && FCGIWRAP_PREFORK=5
+[[ -z $FCGIWRAP_PREFORK ]] && FCGIWRAP_PREFORK=5
+[[ -z $FCGIWRAP_SOCKET  ]] && FCGIWRAP_SOCKET="tcp:127.0.0.1:9012"
 
 echo "env config:"
 echo "    FCGIWRAP_PREFORK = $FCGIWRAP_PREFORK"
+echo "    FCGIWRAP_SOCKET  = $FCGIWRAP_SOCKET "
 echo
 
 set -x
-exec fcgiwrap -s tcp:127.0.0.1:9012 -c $FCGIWRAP_PREFORK
+exec fcgiwrap -s $FCGIWRAP_SOCKET -c $FCGIWRAP_PREFORK

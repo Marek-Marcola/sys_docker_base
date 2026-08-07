6433e59a9d6491bb9ac5052f265cdd59de380f95
Marek Marcola <Marek.Marcola@gmail.com>
2026-08-07 17:34:01 +0200

    fio: add run script
    Change-Id: Ib6367536318af35b070cecf788f1b033f3163d8f

---
+++ fio/scripts/200-ep-fio.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[[ -z $FIO_OPTS ]] && FIO_OPTS=""
+
+echo "env config:"
+echo "    FIO_OPTS = $FIO_OPTS"
+echo
+
+if [ -z "$FIO_OPTS" ]; then
+  echo Empty FIO_OPTS - running shell ...
+  /bin/bash -l
+  exit 0
+fi
+
+set -x
+fio --version
+exec fio $FIO_OPTS
